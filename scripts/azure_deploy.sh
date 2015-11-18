8828effcc7d6f9409167bb0d1f014773d4129fc1
Erik Schlegel <erik.schlegel@gmail.com>
2015-11-18 10:52:14 -0500

    Fixing deployment issue

--- scripts/azure_deploy.sh
+++ scripts/azure_deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-
+cd _site
 git init
 
+echo 'Setting up deployment config'
 git config user.name "Travis CI"
 git config user.email "erisch@microsoft.com"
 
+echo 'Adding files to local repo'
 git add .
 git commit -m "Deploy"
 
 # We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+echo 'About to push to ${AZURE_WA_GIT_TARGET}'
 git push --force --quiet "https://${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD}@${AZURE_WA_GIT_TARGET}" master:master > /dev/null 2>&1
+echo 'Deployed!!''
