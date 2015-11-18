e52df3948dbe098930ecd9876497e1a622b2b885
Erik Schlegel <erik.schlegel@gmail.com>
2015-11-18 14:05:45 -0500

    Update travis_deploy.sh

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -15,5 +15,5 @@ git commit -m "Deploy"
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 echo "About to push to ${AZURE_WA_GIT_TARGET}"
 
-git push --force "https://${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD}@${AZURE_WA_GIT_TARGET}" master:master
+git push --force -v "https://${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD}@${AZURE_WA_GIT_TARGET}" master:master
 echo 'Deployed!!'
