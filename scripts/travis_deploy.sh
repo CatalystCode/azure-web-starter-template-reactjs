d498fc237171a8a581b0acb153430b10a138224f
Erik Schlegel <erik.schlegel@gmail.com>
2015-11-18 15:24:21 -0500

    Update travis_deploy.sh

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -2,19 +2,22 @@
 
 git init
 
+GIT_TARGET_URL="https://${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD}@${AZURE_WA_GIT_TARGET}"
+
+git remote add azure $GIT_TARGET_URL
+
 echo 'Setting up deployment config'
 git config user.name "Travis CI"
 git config user.email "erisch@microsoft.com"
 
 echo 'Adding files to local repo '
 ls -ltr
-git add .
+git add --force .
 git commit -m "Deploy"
-git diff --stat --cached origin/master
 
 # We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 echo "About to push to ${AZURE_WA_GIT_TARGET}"
 
-git push --force -v "https://${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD}@${AZURE_WA_GIT_TARGET}" origin:master
+git push --force azure master
 echo 'Deployed!!'
